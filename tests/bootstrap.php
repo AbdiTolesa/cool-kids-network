222af54977737a2c546caed94df592e51274448d
Abdi Tolessa <41271840+AbdiTolesa@users.noreply.github.com>
2025-02-01 12:48:47 +0300

    Setup PHPUnit test

---
+++ tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'cool-kids-network/cool-kids-network.php' ),
+);
+
+echo 'Welcome to the Formidable Forms Test Suite' . PHP_EOL;
+echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
+
+if ( ! defined( 'SCRIPT_DEBUG' ) ) {
+	define( 'SCRIPT_DEBUG', false );
+}
+
+if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
+	include __DIR__ . '/../vendor/autoload.php';
+}
+
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
+} else {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
+}
---
+++ tests/test_Users.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @since 1.0
+ */
+class test_Users extends WP_UnitTestCase {
+	public function test_generate_username_from_name() {
+		$first_name = 'John';
+		$last_name  = 'Doe';
+		$username   = CoolKidsNetwork\Users::generate_username_from_name( $first_name, $last_name );
+
+		$this->assertEquals( 'john.doe', $username, 'If the username does not exist, return the first name and last name separated by a period.' );
+
+		$user_id = $this->factory->user->create( array( 'user_login' => $username ) );
+		$username = CoolKidsNetwork\Users::generate_username_from_name( $first_name, $last_name );
+
+		$this->assertEquals( 'john.doe1', $username, 'If the username exists, append a number to the username.' );
+	}
+}
